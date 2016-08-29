3001e3ff56fb4a5ce232825a78e51a93cade28c3
Bruno Sendras <brunosendras@gmail.com>
2016-08-29 11:54:51 -0300

    Reemplazados algunos espacios por tabs.

--- ejercicio4/ejercicio4.ps1
+++ ejercicio4/ejercicio4.ps1
@@ -36,39 +36,39 @@ if ($print) {
 	.DESCRIPTION
 	Este script permita filtrar, ordenar y opcionalmente mostrar por pantalla una colección de objetos.
 	Posee dos modos de uso: 
-    1- Mostrar por pantalla las propiedades seleccionadas de los objetos que coinciden con la búsqueda.
-    2- Filtrar, ordenar y Entregar como salida la lista de objetos que coinciden con la búsqueda.
-    Todos los parametros de este script parametros deben ser referenciados por nombre.
+	1- Mostrar por pantalla las propiedades seleccionadas de los objetos que coinciden con la búsqueda.
+	2- Filtrar, ordenar y Entregar como salida la lista de objetos que coinciden con la búsqueda.
+	Todos los parametros de este script parametros deben ser referenciados por nombre.
 	.PARAMETER input
 	Lista de objetos de entrada. Debe recibirse a través de un pipeline.
 	.PARAMETER propiedad
 	Nombre de la propiedad por la que se desea filtrar.
-    .PARAMETER filtro
-    Palabra clave utilizada como criterio de búsqueda. El texto de este argumento debe ser igual o estar contenido en el valor de la propiedad indicada en -propiedad. 
-    .PARAMETER desc
-    Ordena la lista de objetos obtenida de forma descendente según la propiedad indicada en -propiedad. No puede ser utilizado al mismo tiempo que -asc.
-    .PARAMETER asc
-    (Default) Ordena la lista de objetos obtenida de forma ascendente según la propiedad indicada en -propiedad. No puede ser utilizado al mismo tiempo que -desc.
-    .PARAMETER print
-    Si está presente, debe indicarse como argumento una lista de propiedades. El script solo mostrará por pantalla las propiedades seleccionadas de los objetos que coinciden con la búsqueda. No se realiza ordenamiento.
+	.PARAMETER filtro
+	Palabra clave utilizada como criterio de búsqueda. El texto de este argumento debe ser igual o estar contenido en el valor de la propiedad indicada en -propiedad. 
+	.PARAMETER desc
+	Ordena la lista de objetos obtenida de forma descendente según la propiedad indicada en -propiedad. No puede ser utilizado al mismo tiempo que -asc.
+	.PARAMETER asc
+	(Default) Ordena la lista de objetos obtenida de forma ascendente según la propiedad indicada en -propiedad. No puede ser utilizado al mismo tiempo que -desc.
+	.PARAMETER print
+	Si está presente, debe indicarse como argumento una lista de propiedades. El script solo mostrará por pantalla las propiedades seleccionadas de los objetos que coinciden con la búsqueda. No se realiza ordenamiento.
 	.EXAMPLE
-    Devuelve una lista filtrada de objetos Process. Aquellos que tengan el numero 12 en su ID, ordenados ascendentemente según dicha propiedad.
+	Devuelve una lista filtrada de objetos Process. Aquellos que tengan el numero 12 en su ID, ordenados ascendentemente según dicha propiedad.
 
 	Get-Process | .\ejercicio4.ps1 -propiedad ID -filtro 12 -desc
 	.EXAMPLE
-    Devuelve una lista filtrada de objetos Process. Aquellos que tengan el numero 12 en su ID, ordenados descendentemente según dicha propiedad.
+	Devuelve una lista filtrada de objetos Process. Aquellos que tengan el numero 12 en su ID, ordenados descendentemente según dicha propiedad.
 
 	Get-Process | .\ejercicio4.ps1 -propiedad ID -filtro 12 -asc
 	.EXAMPLE
-    Devuelve una lista filtrada de objetos Process. Aquellos que tengan el numero 12 en su ID, ordenados ascendentemente (por default) según dicha propiedad.
+	Devuelve una lista filtrada de objetos Process. Aquellos que tengan el numero 12 en su ID, ordenados ascendentemente (por default) según dicha propiedad.
 
 	Get-Process | .\ejercicio4.ps1 -propiedad ID -filtro 12
-    .EXAMPLE
-    Filtra una lista de objetos Process. Aquellos que tengan el numero 12 en su ID, y muestra por pantalla las propiedades indicadas en -print para cada resultado obtenido.
-    
+	.EXAMPLE
+	Filtra una lista de objetos Process. Aquellos que tengan el numero 12 en su ID, y muestra por pantalla las propiedades indicadas en -print para cada resultado obtenido.
+
 	Get-Process | .\ejercicio4.ps1 -propiedad ID -filtro 12 -print Name,CPU,Handles
-    .EXAMPLE
-    Devuelve una lista de los objetos que representan archivos con la extensión .txt.
-    
+	.EXAMPLE
+	Devuelve una lista de los objetos que representan archivos con la extensión .txt.
+
 	gci | .\ejercicio4.ps1 -propiedad Name -filtro .txt
 #>
